daa0fece26134192b25e7946ff518e9a52a1c88c
rahulreddybiski <rahulreddy.biski@gmail.com>
2022-07-26 15:17:35 +0530

    htms and css added

---
+++ script.js
@@ -0,0 +1,17 @@
+class Calculator{
+    Calculator()
+}
+
+
+
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperationText = document.querySelector('[data-previuos-operand]')
+const currentOperatoinText = document.querySelector('[data-current-operand]')
